baff704c7b18e0d7f6c2035f077c613f533157a0
nbari <nbari@tequila.io>
2017-05-31 02:05:18 +0200

    sync

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func CSRF(next http.Handler) http.Handler {
-	time := time.Now().String()
+	time := time.Now().UTC().String()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("WAF init")
+		log.Println("CSRF init")
 		w.Header().Set("sq-CSRF-version", time)
 		r.Header.Set("sq-CSRF-version", time)
 		next.ServeHTTP(w, r)
